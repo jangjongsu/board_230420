353242a33d476498be47688f3fc21239fdb206f2
HRD <HRD@DESKTOP-MIFHSTI>
2023-04-20 08:52:02 +0900

    board 예제

---
+++ src/main/java/com/jjcompany/board/dao/BDao.java
@@ -0,0 +1,60 @@
+package com.jjcompany.board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BDao {
+	
+	DataSource dataSource;
+
+	public BDao() {
+		super();
+		// TODO Auto-generated constructor stub
+		
+		try {
+			Context comtext = new InitialContext();
+			dataSource = (DataSource) comtext.lookup("jdbc:com/env/jdbc/Orale11g");
+					
+					
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void write(String bname, String btitle, String bcontent) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+				
+				
+		try {
+			conn =dataSource.getConnection();
+			String sql = "insert into mvc_board (bid, bname, btitle, bcontent, bhit, bgroup, bstep, bindent) values (mvc_board_seq.nextval, ?, ?, ?, 0, mvc_board_seq.CURRVAL,0,0)";
+			pstmt= conn.prepareStatement(sql);
+			pstmt.setString(1, bname);
+			pstmt.setString(2, btitle);
+			pstmt.setString(3, bcontent);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+}
