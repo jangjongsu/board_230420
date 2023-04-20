63823f5ab7111a5d4d0fee653759321118e07979
HRD <HRD@DESKTOP-MIFHSTI>
2023-04-20 09:37:30 +0900

    board 예제

--- src/main/java/com/jjcompany/board/controller/BoardController.java
+++ src/main/java/com/jjcompany/board/controller/BoardController.java
@@ -1,15 +1,33 @@
 package com.jjcompany.board.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jjcompany.board.command.BwriteCommand;
 
 @Controller
 public class BoardController {
 	
-	@RequestMapping(value="/write_form")
-	public String write() {
+	@RequestMapping(value="/write_Form")
+	public String writeForm() {
 		
 		return "writeForm";
+	
+	}
+	@RequestMapping(value="/write")
+	public String write(HttpServletRequest request, Model model ) {
+		
+		model.addAttribute("request", request);
+		
+		BwriteCommand command = new BwriteCommand();
+		command.execute(model);
+		
+		
+		
+		return "redirext:list";
 	
 	}
 }
